ee2ed4e7f8dcf86e4c5f8940202e7ef82e33ce76
kekefreedog <kevin.zarshenas@gmail.com>
2023-02-16 00:00:04 +0100

    Update

---
+++ resources/Js/Handlebars/JSONstringify.js
@@ -0,0 +1,20 @@
+/**
+ * Handlebars Comparaison Helpers
+ *
+ * @source https://github.com/helpers/handlebars-helpers
+ * 
+ * @package    kzarshenas/crazyphp
+ * @author     kekefreedog <kevin.zarshenas@gmail.com>
+ * @copyright  2022-2022 Kévin Zarshenas
+ */
+
+/**
+ * JSON stringify
+ * 
+ * Stringify an object using JSON.stringify.
+ * 
+ * @param a Object to stringify
+ * 
+ * @return string
+ */
+module.exports = (a, b, options) => (typeof a === "object") ? JSON.stringify(a) : "";
